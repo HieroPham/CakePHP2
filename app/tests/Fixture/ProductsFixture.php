b1aa30461caf7cf3eae44df5698e1d8d646a3313
Hiero <hiero@cal-se.com>
2024-02-27 10:20:59 +0700

    CakePHP testing

---
+++ app/tests/Fixture/ProductsFixture.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * ProductsFixture
+ */
+class ProductsFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'name' => 'Lorem ipsum dolor sit amet',
+                'price' => 1,
+                'created' => '2024-02-26 08:10:25',
+                'modified' => '2024-02-26 08:10:25',
+            ],
+        ];
+        parent::init();
+    }
+}
